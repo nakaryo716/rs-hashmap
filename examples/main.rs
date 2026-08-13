8a391dbe062182666c4b40a3ca7c23397a33772b
Nakaryo <nakaryo716.dev@gmail.com>
2026-08-13 20:30:14 +0900

    add example code

---
+++ examples/main.rs
@@ -0,0 +1,14 @@
+use rs_hashmap::HashMap;
+
+fn main() {
+    let mut map = HashMap::new(32);
+
+    map.insert("rust", "cargo");
+    map.insert("java", "maven");
+    map.insert("python", "uv");
+
+
+    assert_eq!(map.get("rust"), Some(&"cargo"));
+    assert_eq!(map.get("java"), Some(&"maven"));
+    assert_eq!(map.get("python"), Some(&"uv"));
+}
